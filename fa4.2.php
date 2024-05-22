cd5f953967219786c231cb5f16df2f0f1714a067
Ciscowow <144804969+Ciscowow@users.noreply.github.com>
2024-05-23 02:15:40 +0800

    Update fa4.2.php

--- fa4.2.php
+++ fa4.2.php
@@ -8,28 +8,31 @@
 <style>
     body {
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        font-size: 2px black;
+        font-size: 4px black;
+        align-items: center;
     }
     table {
         border-collapse: collapse;
-        border: 1px solid;   
+        border: 1px solid;  
+        
     }
 
     th, td{
         border: 2px solid;
         text-align: center;
-        padding: 2px;
+        padding: 4px;
     }
 </style>
 <body>
     <?php
-    $names = array('rosmarie tan', 'diwata pares overload', 'daddy tyga', 'fernando the flexer', 'sophia ysabelle tambalong', 'john paul besagas', 'kween lenglang', 'sam angela', 'junjun salarzon', 'kween dura', 'jape tolenda', 'joshua ponenda', 'mr simpatiko', 'michael teodosio','solid nagngangawngaw' );
+    $names = array('rosmarie tan', 'diwata pares overload', 'daddy tyga', 'fernando the flexer', 'sophia ysabelle tambalong', 'john paul besagas', 'kween lenglang', 'sam angela', 'junjun salarzon', 'kween dura', 'jape tolenda', 'joshua ponenda', 'mr simpatiko', 
+    'michael teodosio','solid nagngangawngaw', 'steven smitty', 'evan nganou', 'tyrese haliburton', 'lebron james', 'anthony edwards');
     ?>
     <table>
         <tr>
             <th colspan = '6'><?php 
-            print "List of Names: ";
-            foreach ($names as $n){
+            print "Array List: ";
+            foreach ($names as $n) {
                 print $n . ",";
             }
             ?>
@@ -43,14 +46,15 @@
             <th>Check Position of "a"</th>
             <th>Reverse Name</th>
         </tr>
-        <?php 
         <?php foreach ($names as  $n): ?>
             <tr>
-            <td><?php print $n["name"]; ?></td>
-            <td><img src="<?php print $contacts["image"]; ?>"></td>            
-            <td><?php print $contacts["age"]; ?></td>
-            <td><?php print $contacts["birthday"]; ?></td>
-            <td><?php print $contacts["contact_number"]; ?></td>
+            <td><?php print $n; ?></td>
+            <td><?php print strlen($n); ?></td>
+            <td><?php print ucfirst($n); ?></td>
+            <td><?php print str_replace(array('a', 'e', 'i', 'o', 'u'), '@', $n); ?></td>
+            <td><?php $pos = strpos ($n, 'a');
+                      print $pos !== false ? $pos : 0 ?> </td>
+            <td><?php print strrev($n); ?></td>
             </tr>
         <?php endforeach; ?>
 </body>
